fixed sign function

bitorch/layers/Sign.py
@@ -9,7 +9,7 @@ class SignFunction(Function):
     """Sign Function for input binarization."""
 
     @staticmethod
-    def _sign(tensor):
+    def _sign(tensor: torch.Tensor) -> torch.Tensor:
         """Apply the sign method on the input tensor.
 
         Note: this function will assign 1 as the sign of 0.
@@ -20,12 +20,13 @@ class SignFunction(Function):
         Returns:
             torch.Tensor: the sign tensor
         """
+
         sign_tensor = torch.sign(tensor)
         sign_tensor[sign_tensor == 0] = 1
         return sign_tensor
 
     @staticmethod
-    def forward(ctx, input_tensor):
+    def forward(ctx: any, input_tensor: torch.Tensor, threshold: float = 1.0) -> torch.Tensor:
         """Binarize input tensor using the _sign function.
 
         Args:
@@ -34,10 +35,11 @@ class SignFunction(Function):
         Returns:
             tensor: binarized input tensor
         """
+        ctx.save_for_backward(input_tensor, torch.tensor(threshold))
         return SignFunction._sign(input_tensor)
 
     @staticmethod
-    def backward(ctx, output_grad):
+    def backward(ctx: any, output_grad: torch.Tensor) -> torch.Tensor:
         """Apply straight through estimator.
 
         This passes the output gradient as input gradient after clamping the gradient values to the range [-1, 1]
@@ -49,12 +51,16 @@ class SignFunction(Function):
         Returns:
             torch.Tensor: the input gradient (= the clamped output gradient)
         """
-        return torch.clamp(output_grad, -1, 1)
+        input_tensor, threshold = ctx.saved_tensors
+        # produces zeros where preactivation inputs exceeded threshold, ones otherwise
+        input_grad = (torch.abs(input_tensor) < threshold)
+        return input_grad * output_grad, None
+        # return torch.clamp(input_tensor * output_grad, -1, 1)
 
 
 class Sign(nn.Module):
-    def __init__(self):
+    def __init__(self) -> None:
         super(Sign, self).__init__()
 
-    def forward(self, x):
-        return SignFunction.apply(x)
+    def forward(self, x: torch.Tensor, t: float) -> torch.Tensor:
+        return SignFunction.apply(x, t)

tests/layers/test_sign.py
@@ -1,10 +1,8 @@
-from math import exp
-from bitorch.layers.Sign import SignFunction
+from bitorch.layers.Sign import Sign
 import torch
-from torch.autograd import gradcheck
 import pytest
 
-sign = SignFunction()
+sign = Sign()
 
 TEST_INPUT_DATA = [
     ([1, 2, 3, 4, 5, 6], [1] * 6),
@@ -17,21 +15,23 @@ TEST_INPUT_DATA = [
     ([[[3]], [[-3]], [[0]]], [[[1]], [[-1]], [[1]]])
 ]
 
-TEST_OUTPUT_DATA = [
-    ([1, 2, 3, 4, 5, 6], [1] * 6),
-    ([-1, -2, -3, -4, -5, -6], [-1] * 6),
-    ([0], [0]),
-    ([-0.5, -0.1, 0.1, -0.5], [-0.5, -0.1, 0.1, -0.5]),
-    ([1e-10, 1e-11, 1e-12, -1e-10, -1e-11, -1e-12, 0], [1e-10, 1e-11, 1e-12, -1e-10, -1e-11, -1e-12, 0]),
-    ([1e10, 1e11, 1e12, -1e10, -1e11, -1e12, 0], [1, 1, 1, -1, -1, -1, 0]),
-]
-
 
 @pytest.mark.parametrize("input_values, expected_output", TEST_INPUT_DATA)
-def test_forward_pass(input_values, expected_output):
-    assert torch.equal(sign.forward(None, torch.Tensor(input_values)), torch.Tensor(expected_output))
+@pytest.mark.parametrize("threshold", [0.1, 0.5, 1.0, 2.0, 4.0])
+def test_sign_function(input_values: list, expected_output: list, threshold: float) -> None:
+    x = torch.tensor(input_values).float().requires_grad_(True)
+    x_exp = torch.tensor(expected_output).float().requires_grad_(True)
 
+    y = sign(x, threshold)
+    assert torch.equal(y, x_exp)
 
-@pytest.mark.parametrize("input_values, expected_output", TEST_OUTPUT_DATA)
-def test_backward_pass(input_values, expected_output):
-    assert torch.equal(sign.backward(None, torch.Tensor(input_values)), torch.Tensor(expected_output))
+    # now test gradient cancellation
+    y.backward(x)
+    computed_gradient = x.grad.clone()
+
+    preactivation_input = x.clone().detach().requires_grad_(False)
+    preactivation_input[abs(preactivation_input) >= threshold] = 0
+    # preactivation_input = torch.clamp(preactivation_input, min=-1., max=1.)
+    print(computed_gradient)
+    print(preactivation_input)
+    assert torch.equal(computed_gradient, preactivation_input)