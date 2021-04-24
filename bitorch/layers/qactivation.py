ab0d5683bbca3ad3becc22db7a373a8695a1b7eb
Snagnar <overkill98@web.de>
2021-04-24 19:23:46 +0200

    united activation functions with quantization functions

--- bitorch/layers/qactivation.py
+++ bitorch/layers/qactivation.py
@@ -7,7 +7,7 @@ from . import layerconfig
 class QActivation(nn.Module):
     def __init__(self, activation: str = None) -> None:
         super(QActivation, self).__init__()
-        self.activation = layerconfig.config.get_activation_function(activation)
+        self.activation = layerconfig.config.get_quantization_function(activation)
 
     def forward(self, input_tensor: torch.Tensor) -> torch.Tensor:
         # grad cancel on every quantization function? always ste?
--- bitorch/layers/qdense.py
+++ bitorch/layers/qdense.py
@@ -9,12 +9,16 @@ from . import layerconfig
 
 class QDense(Linear):
     def __init__(self, *kargs, quantization: str = None, **kwargs):  # type: ignore
+        """Applys the given quantization function on weights before applying the linear operation.
+
+        Args:
+            quantization (str, optional): Name of quantization function. Defaults to None.
+        """
         super(QDense, self).__init__(*kargs, **kwargs)
         self.quantize = layerconfig.config.get_quantization_function(quantization)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """Forwards x through the binary linear layer. This code is inspired from the binarizedlinear-implementation at
-        https://github.com/itayhubara/BinaryNet.pytorch/blob/master/models/binarized_modules.py
+        """Forwards x through the binary linear layer.
 
         Args:
             x (torch.Tensor): tensor to forward
