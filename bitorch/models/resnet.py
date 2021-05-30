1be1c67daacec0eeda9a9df20bdf76c5fe625e6a
Snagnar <overkill98@web.de>
2021-05-30 16:40:49 +0200

    added resnet stub

---
+++ bitorch/models/resnet.py
@@ -0,0 +1,21 @@
+from torch.nn import Module
+
+
+class BasicBlockV1(Module):
+    def __init__(self, input_size, output_size, num_layers):
+        pass
+
+
+class BottleneckV1(Module):
+    def __init__(self, input_size, output_size, num_layers):
+        pass
+
+
+class BasicBlockV2(Module):
+    def __init__(self, input_size, output_size, num_layers):
+        pass
+
+
+class BottleneckV2(Module):
+    def __init__(self, input_size, output_size, num_layers):
+        pass
