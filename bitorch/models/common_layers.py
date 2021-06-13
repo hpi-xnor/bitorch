fixed bug with vanishing gradient with resnet

bitorch/models/common_layers.py
@@ -1,4 +1,5 @@
 from torch import nn
+from bitorch.layers import Shape_Print_Debug_Layer
 
 
 def make_initial_layers(variant: str, input_channels: int, output_channels: int) -> nn.Module:
@@ -8,7 +9,11 @@ def make_initial_layers(variant: str, input_channels: int, output_channels: int)
                               kernel_size=7, stride=2, padding=3, bias=False))
     elif variant == "mnist":
         initial_layers.append(nn.Conv2d(input_channels, output_channels,
-                              kernel_size=3, stride=2, padding=1, bias=False))
+                                        kernel_size=(7, 7),
+                                        stride=(2, 2),
+                                        padding=(3, 3), bias=False))
+        # initial_layers.append(nn.Conv2d(input_channels, output_channels,
+        #                       kernel_size=3, stride=1, padding=1, bias=False))
     initial_layers.append(nn.BatchNorm2d(output_channels))
     initial_layers.append(nn.ReLU())
     initial_layers.append(nn.MaxPool2d(kernel_size=3, stride=2, padding=1))

bitorch/models/resnet.py
@@ -1,3 +1,5 @@
+from bitorch.layers.qconv_noact import QConv2d_NoAct
+from bitorch.layers.debug_layers import Input_Print_Debug_Layer, Shape_Print_Debug_Layer
 import torch
 from torch import nn
 from torch.nn import Module
@@ -7,7 +9,19 @@ from bitorch.models.common_layers import make_initial_layers
 
 
 class BasicBlockV1(Module):
-    def __init__(self, in_channels, out_channels, stride):
+    """BasicBlock V1 from `"Deep Residual Learning for Image Recognition"
+    <http://arxiv.org/abs/1512.03385>`_ paper.
+    This is used for ResNet V1 for 18, 34 layers.
+    """
+
+    def __init__(self, in_channels: int, out_channels: int, stride: int) -> None:
+        """builds body and downsampling layers
+
+        Args:
+            in_channels (int): input channels for building block
+            out_channels (int): output channels for building block
+            stride (int): stride to use in convolutions
+        """
         super(BasicBlockV1, self).__init__()
         self.in_channels = in_channels
         self.out_channels = out_channels
@@ -19,13 +33,24 @@ class BasicBlockV1(Module):
         self.body = self._build_body()
         self.activation = nn.ReLU()
 
-    def _build_downsampling(self):
+    def _build_downsampling(self) -> nn.Sequential:
+        """builds the downsampling layers for rediual tensor processing
+
+        Returns:
+            nn.Sequential: the downsampling model
+        """
         return nn.Sequential(
             QConv2d(self.in_channels, self.out_channels, kernel_size=1, stride=self.stride, padding=0),
             nn.BatchNorm2d(self.out_channels),
         )
 
-    def _build_body(self):
+    def _build_body(self) -> nn.Sequential:
+        """builds body of building block, i.e. two binary convolutions with batchnorms in between. Check referenced paper for
+        more details.
+
+        Returns:
+            nn.Sequential: the basic building block body model
+        """
         return nn.Sequential(
             QConv2d(self.in_channels, self.out_channels, kernel_size=3, stride=self.stride, padding=1),
             nn.BatchNorm2d(self.out_channels),
@@ -33,17 +58,37 @@ class BasicBlockV1(Module):
             nn.BatchNorm2d(self.out_channels),
         )
 
-    def forward(self, x: torch.Tensor):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """forwards the input tensor x through the building block.
+
+        Args:
+            x (torch.Tensor): the input tensor
+
+        Returns:
+            torch.Tensor: the output of this building block after adding the input tensor as residual value.
+        """
         residual = x
-        x = self.body(x)
         if self.shall_downsample:
-            residual = self.downsample(residual)
-        x = self.activation(x + residual)
-        return x
+            residual = self.downsample(x)
+        x = self.body(x)
+
+        return x + residual
 
 
 class BottleneckV1(Module):
-    def __init__(self, in_channels, out_channels, stride):
+    """Bottleneck V1 from `"Deep Residual Learning for Image Recognition"
+    <http://arxiv.org/abs/1512.03385>`_ paper.
+    This is used for ResNet V1 for 50, 101, 152 layers.
+    """
+
+    def __init__(self, in_channels: int, out_channels: int, stride: int) -> None:
+        """builds body and downsampling layers
+
+        Args:
+            in_channels (int): input channels for building block
+            out_channels (int): output channels for building block
+            stride (int): stride to use in convolutions
+        """
         super(BottleneckV1, self).__init__()
         self.in_channels = in_channels
         self.out_channels = out_channels
@@ -55,25 +100,44 @@ class BottleneckV1(Module):
         self.body = self._build_body()
         self.activation = nn.ReLU()
 
-    def _build_downsampling(self):
+    def _build_downsampling(self) -> nn.Sequential:
+        """builds the downsampling layers for rediual tensor processing
+
+        Returns:
+            nn.Sequential: the downsampling model
+        """
         return nn.Sequential(
-            QConv2d(self.in_channels, self.out_channels, kernel_size=1, stride=self.stride, padding=0, bias=False),
+            QConv2d_NoAct(self.in_channels, self.out_channels, kernel_size=1,
+                          stride=self.stride, padding=0, bias=False),
             nn.BatchNorm2d(self.out_channels),
         )
 
-    def _build_body(self):
+    def _build_body(self) -> nn.Sequential:
+        """builds body of building block. Check referenced paper for more details.
+
+        Returns:
+            nn.Sequential: the bottleneck body model
+        """
         return nn.Sequential(
-            QConv2d(self.in_channels, self.out_channels // 4, kernel_size=1, stride=self.stride),
+            QConv2d_NoAct(self.in_channels, self.out_channels // 4, kernel_size=1, stride=self.stride),
             nn.BatchNorm2d(self.out_channels // 4),
             nn.ReLU(),
-            QConv2d(self.out_channels // 4, self.out_channels // 4, kernel_size=3, stride=1, padding=1),
+            QConv2d_NoAct(self.out_channels // 4, self.out_channels // 4, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(self.out_channels // 4),
             nn.ReLU(),
-            QConv2d(self.out_channels // 4, self.out_channels, kernel_size=1, stride=1),
+            QConv2d_NoAct(self.out_channels // 4, self.out_channels, kernel_size=1, stride=1),
             nn.BatchNorm2d(self.out_channels)
         )
 
-    def forward(self, x: torch.Tensor):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """forwards the input tensor x through the building block.
+
+        Args:
+            x (torch.Tensor): the input tensor
+
+        Returns:
+            torch.Tensor: the output of this building block after adding the input tensor as residual value.
+        """
         residual = x
         x = self.body(x)
         if self.shall_downsample:
@@ -83,7 +147,20 @@ class BottleneckV1(Module):
 
 
 class BasicBlockV2(Module):
-    def __init__(self, in_channels, out_channels, stride):
+    """BasicBlock V2 from
+    `"Identity Mappings in Deep Residual Networks"
+    <https://arxiv.org/abs/1603.05027>`_ paper.
+    This is used for ResNet V2 for 18, 34 layers.
+    """
+
+    def __init__(self, in_channels: int, out_channels: int, stride: int) -> None:
+        """builds body and downsampling layers
+
+        Args:
+            in_channels (int): input channels for building block
+            out_channels (int): output channels for building block
+            stride (int): stride to use in convolutions
+        """
         super(BasicBlockV2, self).__init__()
         self.in_channels = in_channels
         self.out_channels = out_channels
@@ -95,17 +172,35 @@ class BasicBlockV2(Module):
         self.body = self._build_body()
         self.bn = nn.BatchNorm2d(self.in_channels)
 
-    def _build_downsampling(self):
+    def _build_downsampling(self) -> QConv2d:
+        """builds the downsampling layers for rediual tensor processing
+
+        Returns:
+            QConv2d: the downsampling convolution layer
+        """
         return QConv2d(self.in_channels, self.out_channels, kernel_size=1, stride=self.stride, padding=0)
 
-    def _build_body(self):
+    def _build_body(self) -> nn.Sequential:
+        """builds body of building block. Check referenced paper for more details.
+
+        Returns:
+            nn.Sequential: the bottleneck body model
+        """
         return nn.Sequential(
             QConv2d(self.in_channels, self.out_channels, kernel_size=3, stride=self.stride, padding=1),
             nn.BatchNorm2d(self.out_channels),
             QConv2d(self.out_channels, self.out_channels, kernel_size=3, stride=1, padding=1),
         )
 
-    def forward(self, x: torch.Tensor):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """forwards the input tensor x through the building block.
+
+        Args:
+            x (torch.Tensor): the input tensor
+
+        Returns:
+            torch.Tensor: the output of this building block after adding the input tensor as residual value.
+        """
         bn = self.bn(x)
         if self.shall_downsample:
             residual = self.downsample(bn)
@@ -116,7 +211,14 @@ class BasicBlockV2(Module):
 
 
 class BottleneckV2(Module):
-    def __init__(self, in_channels, out_channels, stride):
+    def __init__(self, in_channels: int, out_channels: int, stride: int) -> None:
+        """builds body and downsampling layers
+
+        Args:
+            in_channels (int): input channels for building block
+            out_channels (int): output channels for building block
+            stride (int): stride to use in convolutions
+        """
         super(BottleneckV2, self).__init__()
         self.in_channels = in_channels
         self.out_channels = out_channels
@@ -129,21 +231,39 @@ class BottleneckV2(Module):
         self.bn = nn.BatchNorm2d(self.in_channels)
         self.activation = nn.ReLU()
 
-    def _build_downsampling(self):
-        return QConv2d(self.in_channels, self.out_channels, kernel_size=1, stride=self.stride, bias=False)
+    def _build_downsampling(self) -> QConv2d_NoAct:
+        """builds the downsampling layers for rediual tensor processing
 
-    def _build_body(self):
+        Returns:
+            QConv2d: the downsampling convolution layer
+        """
+        return QConv2d_NoAct(self.in_channels, self.out_channels, kernel_size=1, stride=self.stride, bias=False)
+
+    def _build_body(self) -> nn.Sequential:
+        """builds body of building block. Check referenced paper for more details.
+
+        Returns:
+            nn.Sequential: the bottleneck body model
+        """
         return nn.Sequential(
-            QConv2d(self.in_channels, self.out_channels // 4, kernel_size=1, stride=self.stride),
+            QConv2d_NoAct(self.in_channels, self.out_channels // 4, kernel_size=1, stride=self.stride),
             nn.BatchNorm2d(self.out_channels // 4),
             nn.ReLU(),
-            QConv2d(self.out_channels // 4, self.out_channels // 4, kernel_size=3, stride=1, padding=1),
+            QConv2d_NoAct(self.out_channels // 4, self.out_channels // 4, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(self.out_channels // 4),
             nn.ReLU(),
-            QConv2d(self.out_channels // 4, self.out_channels, kernel_size=1, stride=1),
+            QConv2d_NoAct(self.out_channels // 4, self.out_channels, kernel_size=1, stride=1),
         )
 
-    def forward(self, x: torch.Tensor):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """forwards the input tensor x through the building block.
+
+        Args:
+            x (torch.Tensor): the input tensor
+
+        Returns:
+            torch.Tensor: the output of this building block after adding the input tensor as residual value.
+        """
         residual = x
         x = self.bn(x)
         x = self.activation(x)
@@ -154,19 +274,49 @@ class BottleneckV2(Module):
 
 
 class ResNet(Module):
-    def __init__(self, classes, channels):
+    """Superclass for ResNet models"""
+
+    def __init__(self, classes: int, channels: list) -> None:
+        """builds feature and output layers
+
+        Args:
+            classes (int): number of output classes
+            channels (list): the channels used in the net
+        """
         super(ResNet, self).__init__()
         self.features = nn.Sequential()
         self.output_layer = nn.Linear(channels[-1], classes)
 
-    def make_layer(self, block, layers, in_channels, out_channels, stride):
+    def make_layer(self, block: Module, layers: int, in_channels: int, out_channels: int, stride: int) -> nn.Sequential:
+        """builds a layer by stacking blocks in a sequential models.
+
+        Args:
+            block (Module): the block of which the layer shall consist
+            layers (int): the number of blocks to stack
+            in_channels (int): the input channels of this layer
+            out_channels (int): the output channels of this layer
+            stride (int): the stride to be used in the convolution layers
+
+        Returns:
+            nn.Sequential: the model containing the building blocks
+        """
         layer_list = []
         layer_list.append(block(in_channels, out_channels, stride))
         for _ in range(layers - 1):
             layer_list.append(block(out_channels, out_channels, 1))
         return nn.Sequential(*layer_list)
 
-    def make_feature_layers(self, block, layers, channels):
+    def make_feature_layers(self, block: Module, layers: list, channels: int) -> nn.Sequential:
+        """builds the given layers with the specified block.
+
+        Args:
+            block (Module): the block of which the layer shall consist
+            layers (list): the number of blocks each layer shall consist of
+            channels (int): the channels 
+
+        Returns:
+            nn.Sequential: [description]
+        """
         feature_layers = []
         for idx, num_layer in enumerate(layers):
             stride = 1 if idx == 0 else 2
@@ -197,9 +347,11 @@ class ResNetV1(ResNet):
         feature_layers.append(nn.BatchNorm2d(image_channels))
         feature_layers.append(make_initial_layers(initial_layers, image_channels, channels[0]))
         feature_layers.append(nn.BatchNorm2d(channels[0]))
+        # feature_layers.append(Input_Print_Debug_Layer(name="before feature layers"))
 
         feature_layers.append(self.make_feature_layers(block, layers, channels))
 
+        # feature_layers.append(Input_Print_Debug_Layer(name="after feature layers"))
         feature_layers.append(nn.ReLU())
         feature_layers.append(nn.AdaptiveAvgPool2d(1))
         feature_layers.append(nn.Flatten())

examples/mnist-resnet.py
@@ -2,9 +2,9 @@ import argparse
 import logging
 import sys
 
-from torchvision.datasets import mnist
+from torchvision.datasets import MNIST
 from torch.utils.data import DataLoader
-from torchvision.transforms import ToTensor
+from torchvision.transforms import Compose, ToTensor, Normalize, Resize
 from torch.nn import CrossEntropyLoss
 
 from train import train_model
@@ -15,11 +15,11 @@ from bitorch.models.resnet import create_resnet
 
 
 def main(args: argparse.Namespace) -> None:
-
-    train_dataset = mnist.MNIST(root='./train', train=True, transform=ToTensor(), download=True)
-    test_dataset = mnist.MNIST(root='./test', train=False, transform=ToTensor(), download=True)
+    train_dataset = MNIST(root='./train', train=True, transform=ToTensor(), download=True)
+    test_dataset = MNIST(root='./test', train=False, transform=ToTensor(), download=True)
     train_loader = DataLoader(train_dataset, batch_size=args.batch_size)
     test_loader = DataLoader(test_dataset, batch_size=args.batch_size)
+
     if args.logging is None:
         logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s', level=logging.DEBUG, force=True)
     else: