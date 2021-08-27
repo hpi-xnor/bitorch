783488c5ab125f694a911b975a47d5a38af48110
Snagnar <overkill98@web.de>
2021-08-27 10:57:25 +0200

    refactored model tests, fixed imagenet shape

--- tests/models/test_models.py
+++ tests/models/test_models.py
@@ -16,71 +16,44 @@ from bitorch.models import (
 import torch
 import numpy as np
 import pytest
+import itertools
+
+mnist_100 = [100, 1, 28, 28]
+mnist_10 = [10, 1, 28, 28]
+cifar_100 = [100, 3, 32, 32]
+cifar_10 = [10, 3, 32, 32]
+imagenet_2 = [2, 3, 224, 224]
+imagenet_1 = [1, 3, 224, 224]
+more_channels_mnist_100 = [100, 10, 28, 28]
 
 TEST_INPUT_DATA = [
-    (Resnet, {"resnet_version": 1, "resnet_num_layers": 18}, (100, 1, 28, 28)),
-    (Resnet, {"resnet_version": 1, "resnet_num_layers": 34}, (100, 1, 28, 28)),
-    (Resnet, {"resnet_version": 1, "resnet_num_layers": 50}, (10, 1, 28, 28)),
-    (Resnet, {"resnet_version": 2, "resnet_num_layers": 18}, (100, 1, 28, 28)),
-    (Resnet, {"resnet_version": 2, "resnet_num_layers": 34}, (100, 1, 28, 28)),
-    (Resnet, {"resnet_version": 2, "resnet_num_layers": 50}, (10, 1, 28, 28)),
-
-    (Resnet18_v1, {}, (100, 1, 28, 28)),
-    (Resnet18_v1, {}, (100, 3, 32, 32)),
-    (Resnet18_v1, {}, (2, 3, 244, 244)),
-
-    (Resnet34_v1, {}, (100, 1, 28, 28)),
-    (Resnet34_v1, {}, (100, 3, 32, 32)),
-    (Resnet34_v1, {}, (2, 3, 244, 244)),
-
-    (Resnet50_v1, {}, (10, 1, 28, 28)),
-    (Resnet50_v1, {}, (10, 3, 32, 32)),
-    (Resnet50_v1, {}, (1, 3, 244, 244)),
-
-    (Resnet18_v2, {}, (100, 1, 28, 28)),
-    (Resnet18_v2, {}, (100, 3, 32, 32)),
-    (Resnet18_v2, {}, (2, 3, 244, 244)),
-
-    (Resnet34_v2, {}, (100, 1, 28, 28)),
-    (Resnet34_v2, {}, (100, 3, 32, 32)),
-    (Resnet34_v2, {}, (2, 3, 244, 244)),
-
-    (Resnet50_v2, {}, (10, 1, 28, 28)),
-    (Resnet50_v2, {}, (10, 3, 32, 32)),
-    (Resnet50_v2, {}, (1, 3, 244, 244)),
-
-    (Resnet_E, {"resnete_num_layers": 18}, (10, 1, 28, 28)),
-    (Resnet_E, {"resnete_num_layers": 34}, (10, 1, 28, 28)),
-
-    (Resnet_E18, {}, (10, 1, 28, 28)),
-    (Resnet_E18, {}, (10, 3, 32, 32)),
-    (Resnet_E18, {}, (2, 3, 244, 244)),
-
-    (Resnet_E34, {}, (10, 1, 28, 28)),
-    (Resnet_E34, {}, (10, 3, 32, 32)),
-    (Resnet_E34, {}, (2, 3, 244, 244)),
-
-    (LeNet, {"lenet_quantized": True}, (100, 1, 28, 28)),
-    (LeNet, {"lenet_quantized": True}, (100, 2, 28, 28)),
-    (LeNet, {"lenet_quantized": True}, (100, 3, 28, 28)),
-    (LeNet, {"lenet_quantized": True}, (100, 10, 28, 28)),
-
-    (LeNet, {"lenet_quantized": False}, (100, 1, 28, 28)),
-    (LeNet, {"lenet_quantized": False}, (100, 2, 28, 28)),
-    (LeNet, {"lenet_quantized": False}, (100, 3, 28, 28)),
-    (LeNet, {"lenet_quantized": False}, (100, 10, 28, 28)),
+    [Resnet, {"resnet_version": [1, 2], "resnet_num_layers": [18, 34, 50]}, [mnist_10, mnist_100, cifar_10]],
+    [Resnet18_v1, {}, [mnist_10, mnist_100, cifar_10, cifar_100, imagenet_1, imagenet_2]],
+    [Resnet34_v1, {}, [mnist_10, mnist_100, cifar_10, cifar_100, imagenet_1, imagenet_2]],
+    [Resnet50_v1, {}, [mnist_10, cifar_10, imagenet_1]],
+    [Resnet18_v2, {}, [mnist_10, mnist_100, cifar_10, cifar_100, imagenet_1, imagenet_2]],
+    [Resnet34_v2, {}, [mnist_10, mnist_100, cifar_10, cifar_100, imagenet_1, imagenet_2]],
+    [Resnet50_v2, {}, [mnist_10, cifar_10, imagenet_1]],
+    [Resnet_E, {"resnete_num_layers": [18, 34]}, [mnist_10, mnist_100, cifar_10]],
+    [Resnet_E18, {}, [mnist_10, mnist_100, cifar_10, cifar_100, imagenet_1, imagenet_2]],
+    [Resnet_E34, {}, [mnist_10, mnist_100, cifar_10, cifar_100, imagenet_1, imagenet_2]],
+    [LeNet, {"lenet_quantized": [True, False]}, [mnist_10, mnist_100, more_channels_mnist_100]]
 ]
 
 
-@pytest.mark.parametrize("model_class, kwargs, input_shape", TEST_INPUT_DATA)
-def test_models(model_class, kwargs, input_shape) -> None:
+@pytest.mark.parametrize("model_class, kwargs, input_shapes", TEST_INPUT_DATA)
+def test_models(model_class, kwargs, input_shapes) -> None:
     dataset = dataset_from_name("cifar10")
     assert dataset is CIFAR10
-    dataset.shape = input_shape
-
     assert models_by_name[model_class.name] is model_class
 
-    model = model_class(dataset=dataset, **kwargs)
-    input_values = torch.Tensor(np.random.uniform(0, 1.0, input_shape))
-    output = model(input_values)
-    assert torch.equal(torch.as_tensor(output.shape), torch.Tensor([input_shape[0], dataset.num_classes]).long())
+    kwarg_combinations = [dict(zip(kwargs.keys(), combination)) for combination in itertools.product(*kwargs.values())]
+
+    for combination in kwarg_combinations:
+        for input_shape in input_shapes:
+            dataset.shape = input_shape
+            model = model_class(dataset=dataset, **combination)
+            input_values = torch.Tensor(np.random.uniform(0, 1.0, input_shape))
+            output = model(input_values)
+            assert torch.equal(torch.as_tensor(output.shape), torch.Tensor(
+                [input_shape[0], dataset.num_classes]).long())
