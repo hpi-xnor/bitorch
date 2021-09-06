699754c828e8d876bb368036ab24bfd034dd007a
Snagnar <overkill98@web.de>
2021-09-06 12:27:16 +0200

    added NotImplementedError in dataset

--- bitorch/datasets/base.py
+++ bitorch/datasets/base.py
@@ -1,5 +1,5 @@
-from typing import Tuple
 import torch
+from typing import Tuple
 from torch.utils.data import Dataset
 from enum import Enum
 
@@ -37,18 +37,55 @@ class BasicDataset(Dataset):
             self._dataset = self.augment_dataset(self._dataset, augmentation)
 
     def get_dataset(self, train: bool, directory: str, download: bool) -> Dataset:
-        return Dataset()
+        """creates the dataset, has to respond to train flag, i.e. if train true the train dataset is to be created,
+        if false the test dataset.
+
+        Args:
+            train (bool): toggles if train or test dataset shall be created
+            directory (str): path to test/train dataset store dir (optional)
+            download (bool): toggles if train/test shall be downloaded if possible
+
+        Raises:
+            NotImplementedError: thrown, because this method needs to be overwritten by subclasses
+
+        Returns:
+            Dataset: the created test/train dataset
+        """
+        raise NotImplementedError()
 
     def transform(self, x: torch.Tensor) -> torch.Tensor:
+        """transforms the data tensor x. this method is called lazily every time an item is fetched.
+
+        Args:
+            x (torch.Tensor): the input tensor to be transformed
+
+        Returns:
+            torch.Tensor: the transformed tensor
+        """
         return x
 
     def augment_dataset(self, dataset: Dataset, augmentation_level: Augmentation = Augmentation.NONE) -> Dataset:
+        """custom dataset augmentation can be implemented here.
+
+        Args:
+            dataset (Dataset): the dataset to be augmented
+            augmentation_level (Augmentation, optional): the augmentation level (4 different levels available).
+                Defaults to Augmentation.NONE.
+
+        Returns:
+            Dataset: the augmented dataset
+        """
         return dataset
 
-    def add_argparse_arguments(parser) -> None:
-        pass
-
     def __getitem__(self, index: int) -> Tuple[torch.Tensor, torch.Tensor]:  # type: ignore
+        """returns the item on the specified index after applying the transform opteration.
+
+        Args:
+            index (int): requested index
+
+        Returns:
+            Tuple[torch.Tensor, torch.Tensor]: the data and lable at the specified index
+        """
         item = self._dataset[index]
         if isinstance(item, tuple):
             return self.transform(item[0]), item[1]
