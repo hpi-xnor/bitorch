36ca7eff5b99dfac51ca950e42e5b73259cdf8c6
Snagnar <overkill98@web.de>
2021-10-08 14:38:34 +0200

    first draft of moving grad cancellation into qactivation

--- bitorch/layers/qactivation.py
+++ bitorch/layers/qactivation.py
@@ -1,15 +1,61 @@
-from typing import Union
-from bitorch.quantizations import Quantization
+import typing
+from typing import Union, Tuple
 import torch
 from torch import nn
+from torch.autograd.function import Function
 
+from bitorch.quantizations import Quantization
 from .config import config
+
+
+class GradientCancellation(Function):
+
+    @staticmethod
+    @typing.no_type_check
+    def forward(
+            ctx: torch.autograd.function.BackwardCFunction,  # type: ignore
+            input_tensor: torch.Tensor,
+            threshold: float = 1.0) -> torch.Tensor:
+        """Binarize input tensor using the _sign function.
+
+        Args:
+            input_tensor (tensor): the input values to the Sign function
+
+        Returns:
+            tensor: binarized input tensor
+        """
+        ctx.save_for_backward(input_tensor, torch.tensor(threshold, device=input_tensor.device))
+        return input_tensor
+
+    @staticmethod
+    @typing.no_type_check
+    def backward(
+            ctx: torch.autograd.function.BackwardCFunction,  # type: ignore
+            output_grad: torch.Tensor) -> Tuple[torch.Tensor, None]:
+        """Apply straight through estimator.
+
+        This passes the output gradient as input gradient after clamping the gradient values to the range [-1, 1]
+
+        Args:
+            ctx (gradient context): context
+            output_grad (toch.Tensor): the tensor containing the output gradient
+
+        Returns:
+            torch.Tensor: the input gradient (= the clamped output gradient)
+        """
+        input_tensor, threshold = ctx.saved_tensors
+        # produces zeros where preactivation inputs exceeded threshold, ones otherwise
+        input_grad = (torch.abs(input_tensor) < threshold)
+        return input_grad * output_grad, None
 
 
 class QActivation(nn.Module):
     """Activation layer for quantization"""
 
-    def __init__(self, activation: Union[str, Quantization] = None) -> None:
+    def __init__(
+            self,
+            activation: Union[str, Quantization] = None,
+            gradient_cancellation_threshold: Union[float, None] = None) -> None:
         """initialization function for fetching suitable activation function.
 
         Args:
@@ -17,7 +63,8 @@ class QActivation(nn.Module):
                 Defaults to None.
         """
         super(QActivation, self).__init__()
-        self.activation = config.get_quantization_function(activation)
+        self._activation = config.get_quantization_function(activation)
+        self._gradient_cancellation_threshold = gradient_cancellation_threshold
 
     def forward(self, input_tensor: torch.Tensor) -> torch.Tensor:
         """Forwards input tensor through activation function.
@@ -28,4 +75,8 @@ class QActivation(nn.Module):
         Returns:
             torch.Tensor: quantized input tensor.
         """
-        return self.activation(input_tensor)
+        activated_input = self._activation(input_tensor)
+        if self._gradient_cancellation_threshold:
+            return GradientCancellation.apply(activated_input, self._gradient_cancellation_threshold)
+        else:
+            return activated_input
--- bitorch/quantizations/sign.py
+++ bitorch/quantizations/sign.py
@@ -1,70 +1,70 @@
 """Sign Function Implementation"""
 import torch
-import typing
-from typing import Tuple, Union
-from torch.autograd import Function
+# import typing
+from typing import Union
+# from torch.autograd import Function
 
 from .base import Quantization
 from .config import config
 
 
-class SignFunction(Function):
-    """Sign Function for input binarization."""
+# class SignFunction(Function):
+#     """Sign Function for input binarization."""
 
-    @staticmethod
-    def _sign(tensor: torch.Tensor) -> torch.Tensor:
-        """Apply the sign method on the input tensor.
+#     @staticmethod
+#     def _sign(tensor: torch.Tensor) -> torch.Tensor:
+#         """Apply the sign method on the input tensor.
 
-        Note: this function will assign 1 as the sign of 0.
+#         Note: this function will assign 1 as the sign of 0.
 
-        Args:
-            tensor (torch.Tensor): the tensor to calculate the sign of
+#         Args:
+#             tensor (torch.Tensor): the tensor to calculate the sign of
 
-        Returns:
-            torch.Tensor: the sign tensor
-        """
+#         Returns:
+#             torch.Tensor: the sign tensor
+#         """
 
-        sign_tensor = torch.sign(tensor)
-        sign_tensor[sign_tensor == 0] = 1
-        return sign_tensor
+#         sign_tensor = torch.sign(tensor)
+#         sign_tensor[sign_tensor == 0] = 1
+#         return sign_tensor
 
-    @staticmethod
-    @typing.no_type_check
-    def forward(
-            ctx: torch.autograd.function.BackwardCFunction,  # type: ignore
-            input_tensor: torch.Tensor,
-            threshold: float = 1.0) -> torch.Tensor:
-        """Binarize input tensor using the _sign function.
+#     @staticmethod
+#     @typing.no_type_check
+#     def forward(
+#             ctx: torch.autograd.function.BackwardCFunction,  # type: ignore
+#             input_tensor: torch.Tensor,
+#             threshold: float = 1.0) -> torch.Tensor:
+#         """Binarize input tensor using the _sign function.
 
-        Args:
-            input_tensor (tensor): the input values to the Sign function
+#         Args:
+#             input_tensor (tensor): the input values to the Sign function
 
-        Returns:
-            tensor: binarized input tensor
-        """
-        ctx.save_for_backward(input_tensor, torch.tensor(threshold, device=input_tensor.device))
-        return SignFunction._sign(input_tensor)
+#         Returns:
+#             tensor: binarized input tensor
+#         """
+#         ctx.save_for_backward(input_tensor, torch.tensor(threshold, device=input_tensor.device))
+#         return SignFunction._sign(input_tensor)
 
-    @staticmethod
-    @typing.no_type_check
-    def backward(
-            ctx: torch.autograd.function.BackwardCFunction,  # type: ignore
-            output_grad: torch.Tensor) -> Tuple[torch.Tensor, None]:
-        """Apply straight through estimator.
+#     @staticmethod
+#     @typing.no_type_check
+#     def backward(
+#             ctx: torch.autograd.function.BackwardCFunction,  # type: ignore
+#             output_grad: torch.Tensor) -> Tuple[torch.Tensor, None]:
+#         """Apply straight through estimator.
 
-        This passes the output gradient as input gradient after clamping the gradient values to the range [-1, 1]
+#         This passes the output gradient as input gradient after clamping the gradient values to the range [-1, 1]
 
-        Args:
-            ctx (gradient context): context
-            output_grad (toch.Tensor): the tensor containing the output gradient
+#         Args:
+#             ctx (gradient context): context
+#             output_grad (toch.Tensor): the tensor containing the output gradient
 
-        Returns:
-            torch.Tensor: the input gradient (= the clamped output gradient)
-        """
-        input_tensor, threshold = ctx.saved_tensors
-        # produces zeros where preactivation inputs exceeded threshold, ones otherwise
-        input_grad = (torch.abs(input_tensor) < threshold)
-        return input_grad * output_grad, None
+#         Returns:
+#             torch.Tensor: the input gradient (= the clamped output gradient)
+#         """
+#         input_tensor, threshold = ctx.saved_tensors
+#         # produces zeros where preactivation inputs exceeded threshold, ones otherwise
+#         input_grad = (torch.abs(input_tensor) < threshold)
+#         return input_grad * output_grad, None
 
 
 class Sign(Quantization):
@@ -90,4 +90,6 @@ class Sign(Quantization):
         Returns:
             torch.Tensor: sign of tensor x
         """
-        return SignFunction.apply(x, self.gradient_cancelation_threshold)
+        sign_tensor = torch.sign(x)
+        sign_tensor[sign_tensor == 0] = 1
+        return sign_tensor
