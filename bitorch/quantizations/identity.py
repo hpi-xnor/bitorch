3639820cd0b43f3c3807e3f139ff897276f62bfd
Joseph Bethge <Jopyth@users.noreply.github.com>
2021-10-08 16:23:59 +0200

    add identity quantization function

---
+++ bitorch/quantizations/identity.py
@@ -0,0 +1,21 @@
+"""Identity Implementation"""
+
+from .base import Quantization
+import torch
+
+
+class Identity(Quantization):
+    """Module that provides the identity function, which can be useful for certain training strategies"""
+
+    name = "identity"
+
+    def quantize(self, x: torch.Tensor) -> torch.Tensor:
+        """forwards the input tensor x without quantization.
+
+        Args:
+            x (torch.Tensor): tensor to be forwarded.
+
+        Returns:
+            torch.Tensor: tensor x
+        """
+        return x
