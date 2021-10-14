fixed miscalculation of gradients

bitorch/layers/qactivation.py
@@ -15,7 +15,7 @@ class GradientCancellation(Function):
     def forward(
             ctx: torch.autograd.function.BackwardCFunction,  # type: ignore
             input_tensor: torch.Tensor,
-            threshold: float = 1.0) -> torch.Tensor:
+            threshold: float, activation: Quantization) -> torch.Tensor:
         """Binarize input tensor using the _sign function.
 
         Args:
@@ -25,13 +25,14 @@ class GradientCancellation(Function):
             tensor: binarized input tensor
         """
         ctx.save_for_backward(input_tensor, torch.tensor(threshold, device=input_tensor.device))
-        return input_tensor
+        activated_input = activation(input_tensor)
+        return activated_input
 
     @staticmethod
     @typing.no_type_check
     def backward(
             ctx: torch.autograd.function.BackwardCFunction,  # type: ignore
-            output_grad: torch.Tensor) -> Tuple[torch.Tensor, None]:
+            output_grad: torch.Tensor) -> Tuple[torch.Tensor, None, None]:
         """Apply straight through estimator.
 
         This passes the output gradient as input gradient after clamping the gradient values to the range [-1, 1]
@@ -47,8 +48,8 @@ class GradientCancellation(Function):
         cancelled = torch.where(
             torch.abs(input_tensor) < threshold,
             output_grad,
-            torch.tensor(1., device=output_grad.device))
-        return cancelled, None
+            torch.tensor(0., device=output_grad.device))
+        return cancelled, None, None
 
 
 class QActivation(nn.Module):
@@ -64,7 +65,7 @@ class QActivation(nn.Module):
             activation (Union[str, Quantization], optional): quantization module or name of quantization function.
                 Defaults to None.
             gradient_cancellation_threshold (Union[float, None], optional): threshold for input gradient
-                cancellation. Disabled if threshold is None. Defaults to None.
+                cancellation. Disabled if threshold is None or 0. Defaults to None.
         """
         super(QActivation, self).__init__()
         self._activation = config.get_quantization_function(activation or config.input_quantization())
@@ -81,8 +82,7 @@ class QActivation(nn.Module):
         Returns:
             torch.Tensor: quantized input tensor.
         """
-        activated_input = self._activation(input_tensor)
-        if self._gradient_cancellation_threshold:
-            return GradientCancellation.apply(activated_input, self._gradient_cancellation_threshold)
+        if self._gradient_cancellation_threshold and self._gradient_cancellation_threshold > 0:
+            return GradientCancellation.apply(input_tensor, self._gradient_cancellation_threshold, self._activation)
         else:
-            return activated_input
+            return self._activation(input_tensor)

bitorch/quantizations/dorefa.py
@@ -1,8 +1,10 @@
 """Dorefa Function Implementation"""
 import torch
-from typing import Union
+from typing import Any, Tuple, Union
 
-from .base import Quantization, STE
+from torch.autograd.function import Function
+
+from .base import Quantization
 from .config import config
 
 
@@ -40,6 +42,40 @@ class WeightDoReFa(Quantization):
         return 2.0 * (torch.round(adjusted_values * self._max_value) / self._max_value) - 1.0
 
 
+class InputDoReFaFunction(Function):
+    @staticmethod
+    def forward(
+            ctx: torch.autograd.function.BackwardCFunction,  # type: ignore
+            input_tensor: torch.Tensor, bits: int) -> torch.Tensor:
+        """quantizes input tensor and forwards it.
+
+        Args:
+            ctx (Any): autograd context
+            input_tensor (torch.Tensor): input tensor
+            bits (int): number of bits to round the input tensor to
+
+        Returns:
+            torch.Tensor: the quantized input tensor
+        """
+        max_value = 2 ** bits - 1
+
+        quantized_tensor = torch.round(torch.clamp(input_tensor, 0, 1) * max_value) / max_value
+        return quantized_tensor
+
+    @staticmethod
+    def backward(ctx: Any, output_gradient: torch.Tensor) -> Tuple[torch.Tensor, None]:
+        """just passes the unchanged output gradient as input gradient (i.e. applies straight through estimator)
+
+        Args:
+            ctx (Any): autograd context
+            output_gradient (torch.Tensor): output gradient
+
+        Returns:
+            torch.Tensor: the unchanged output gradient
+        """
+        return output_gradient, None
+
+
 class InputDoReFa(Quantization):
     """Module for applying the dorefa function on inputs.
 
@@ -57,7 +93,6 @@ class InputDoReFa(Quantization):
         """
         super(InputDoReFa, self).__init__()
         self.bits = bits or config.dorefa_bits
-        self._max_value = 2 ** self.bits - 1
 
     def quantize(self, x: torch.Tensor) -> torch.Tensor:
         """DoReFas the tensor to desired bit resolution.
@@ -69,4 +104,4 @@ class InputDoReFa(Quantization):
             torch.Tensor: DoReFaed tensor x
         """
 
-        return STE.apply(torch.round(torch.clamp(x, 0, 1) * self._max_value) / self._max_value)
+        return InputDoReFaFunction.apply(x, self.bits)

bitorch/quantizations/sign.py
@@ -1,6 +1,27 @@
 """Sign Function Implementation"""
 import torch
+# from torch.autograd.function import Function
 from .base import Quantization, STE
+
+
+class SignFunction(STE):
+    @staticmethod
+    def forward(
+            ctx: torch.autograd.function.BackwardCFunction,  # type: ignore
+            input_tensor: torch.Tensor) -> torch.Tensor:
+        """applies sign function
+
+        Args:
+            ctx (Any): autograd context
+            input_tensor (torch.Tensor): input tensor
+
+        Returns:
+            torch.Tensor: the sign tensor
+        """
+        sign_tensor = torch.sign(input_tensor)
+        sign_tensor = torch.where(sign_tensor == 0, torch.tensor(
+            1., device=sign_tensor.device), sign_tensor)
+        return sign_tensor
 
 
 class Sign(Quantization):
@@ -17,6 +38,4 @@ class Sign(Quantization):
         Returns:
             torch.Tensor: sign of tensor x
         """
-        sign_tensor = torch.sign(x)
-        sign_tensor = torch.where(sign_tensor == 0, torch.tensor(1., device=sign_tensor.device), sign_tensor)
-        return STE.apply(sign_tensor)
+        return SignFunction.apply(x)

bitorch/quantizations/steheaviside.py
@@ -3,6 +3,26 @@ import torch
 from .base import STE, Quantization
 
 
+class SteHeavisideFunction(STE):
+    @staticmethod
+    def forward(
+            ctx: torch.autograd.function.BackwardCFunction,  # type: ignore
+            input_tensor: torch.Tensor) -> torch.Tensor:
+        """quantizes input tensor and forwards it.
+
+        Args:
+            ctx (Any): autograd context
+            input_tensor (torch.Tensor): input tensor
+
+        Returns:
+            torch.Tensor: the quantized input tensor
+        """
+
+        quantized_tensor = torch.where(input_tensor > 0, torch.tensor(
+            1., device=input_tensor.device), torch.tensor(0., device=input_tensor.device))
+        return quantized_tensor
+
+
 class SteHeaviside(Quantization):
     """Module for applying the SteHeaviside quantization, using an ste in backward pass"""
 
@@ -17,4 +37,4 @@ class SteHeaviside(Quantization):
         Returns:
             torch.Tensor: sign of tensor x
         """
-        return STE.apply(torch.where(x >= 0, torch.tensor(1., device=x.device), x))
+        return SteHeavisideFunction.apply(x)

tests/layers/test_qactivation.py
@@ -1,4 +1,5 @@
 import pytest
+import torch
 from bitorch.layers.qactivation import QActivation
 from bitorch.layers.config import config
 from bitorch.quantizations import Sign
@@ -6,10 +7,27 @@ from bitorch.quantizations import Sign
 
 activation = QActivation()
 
+TEST_DATA = [-3.0, -1.0, -0.3, 0.0, 0.3, 1.0, 3.0]
+# TEST_THRESHOLDS = [0.0]
+TEST_THRESHOLDS = [0.0, 0.5, 1.0, 2.0]
 
-def test_qactivation():
+
+@pytest.mark.parametrize("threshold", TEST_THRESHOLDS)
+def test_qactivation(threshold):
     assert isinstance(activation._activation, type(config.input_quantization()))
     assert isinstance(QActivation("sign")._activation, Sign)
     assert isinstance(QActivation(Sign())._activation, Sign)
     with pytest.raises(ValueError):
         QActivation("iNvAlIdNaMe")
+
+    x = torch.Tensor(TEST_DATA).float().requires_grad_(True)
+    activation._gradient_cancellation_threshold = threshold
+
+    y = activation(x)
+    y.backward(x)
+
+    if threshold > 0:
+        expected_gradient = torch.where(torch.abs(x) >= threshold, torch.tensor(0.), x)
+    else:
+        expected_gradient = x.clone()
+    assert torch.equal(expected_gradient, x.grad)