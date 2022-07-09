8cc76d920efd48f6b54783ffebec3e4ada0d0188
Joseph Bethge <Jopyth@users.noreply.github.com>
2022-07-09 17:47:29 +0200

    make params of progressive sign function configurable, update tests

--- bitorch/quantizations/config.py
+++ bitorch/quantizations/config.py
@@ -13,5 +13,11 @@ class QuantizationConfig(Config):
     # scaling of progressive sign function, should be zero at the start of the training, and (close to) one at the end
     progressive_sign_scale = 0.0
 
+    # alpha of default progressive sign transform function, should be between 2 and 10
+    progressive_sign_alpha = 2
+
+    # beta of default progressive sign transform function, should be between 2 and 10
+    progressive_sign_beta = 10
+
 
 config = QuantizationConfig()
