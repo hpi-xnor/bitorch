7ce377f4c405402c3f1b615da79dba95142459ab
Snagnar <overkill98@web.de>
2022-11-16 15:25:56 +0100

    first draft of model zoo mechanics

--- bitorch/models/base.py
+++ bitorch/models/base.py
@@ -1,3 +1,4 @@
+import logging
 from argparse import ArgumentParser
 from typing import Optional, List, Any
 
@@ -9,12 +10,14 @@ from bitorch.layers import convert
 from bitorch.layers.qconv1d import QConv1dBase, QConv1d_NoAct
 from bitorch.layers.qconv2d import QConv2dBase, QConv2d_NoAct
 from bitorch.layers.qconv3d import QConv3dBase, QConv3d_NoAct
+from bitorch.util import is_url
 
 
 class Model(nn.Module):
     """Base class for Bitorch models"""
 
     name = ""
+    pretrained_model_url = None
 
     def __init__(self, input_shape: List[int], num_classes: int = 0) -> None:
         super(Model, self).__init__()
@@ -84,6 +87,41 @@ class Model(nn.Module):
 
     def convert(self, new_mode: RuntimeMode, device: Optional[torch.device] = None, verbose: bool = False) -> "Model":
         return convert(self, new_mode, device, verbose)
+
+    def _generate_checkpoint_name(self):
+        # TODO: encode current runtime / layer implementation in name for better reference / correct loading of model
+        return f"{self.name}_checkpoint.pth"
+
+    def from_pretrained(self, source: Optional[str] = None, verbose: bool = False) -> nn.Module:
+        if source is not None:
+            return self._load_from_source(source, verbose)
+        elif self.pretrained_model_url is not None:
+            return self._load_from_url(self.pretrained_model_url)
+        raise ValueError(
+            f"Model {self.name} does not have a predefined download url, "
+            "you have to specify a source to load the model from the specified checkpoint!"
+        )
+
+    def _load_from_url(self, url: str, verbose: bool = False):
+        ...
+
+    def _load_from_source(self, source: str, verbose: bool = False):
+        ...
+
+    def _upload_model(self, source_path: str, destination_url: str):
+        ...
+
+    def store_checkpoint(self, destination: Optional[str] = None) -> None:
+        checkpoint_name = self._generate_checkpoint_name()
+        if is_url(destination):
+            logging.info(f"uploading model to url: {destination}...")
+            tmp_path = f"/tmp/{checkpoint_name}"
+            torch.save(self, tmp_path)
+            self._upload_model(tmp_path, destination)
+        else:
+            logging.debug(f"saving model to {destination}...")
+            torch.save(self, destination)
+        logging.debug("done saving model!")
 
 
 class NoArgparseArgsMixin:
